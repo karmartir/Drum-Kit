397dfd8db52c95c43f8d7e1933e16c8ac355b289
garen <karmartir@gmail.com>
2024-01-20 22:02:46 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+// Detecting Button Press
+
+function audioTest() {
+  const audio = new Audio("sounds/tom-1.mp3");
+  audio.play();
+}
+
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+  });
+}
+
+// Detecting Keyboard Press
+
+document.addEventListener("keypress", function (event) {
+  makeSound(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      audioTest();
+      break;
+
+    case "a":
+      const tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      const tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      const tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      const snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      const crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      const kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    default:
+      console.log(buttonInnerHTML);
+  }
+}
